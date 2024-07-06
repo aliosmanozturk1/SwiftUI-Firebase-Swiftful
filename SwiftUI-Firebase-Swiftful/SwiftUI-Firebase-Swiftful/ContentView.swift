e1501900bdb0e26064df6b116c6a8cbc4c0d495b
Ali Osman Öztürk <developer@aliosmanozturk.me>
2024-07-06 19:23:04 +0300

    Create Project

---
+++ SwiftUI-Firebase-Swiftful/SwiftUI-Firebase-Swiftful/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Firebase-Swiftful
+//
+//  Created by Ali Osman Öztürk on 6.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Firebase-Swiftful/SwiftUI-Firebase-Swiftful/SwiftUI_Firebase_SwiftfulApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Firebase_SwiftfulApp.swift
+//  SwiftUI-Firebase-Swiftful
+//
+//  Created by Ali Osman Öztürk on 6.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Firebase_SwiftfulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
